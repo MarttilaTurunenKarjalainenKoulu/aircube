ee4974899de030c66f1c44380dd1efc5e1edfcd2
Ville Marttila <t3mavi00@students.oamk.fi>
2017-02-16 09:57:23 +0200

    Django working base

--- web/ryhma1/aircube/views.py
+++ web/ryhma1/aircube/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request) :
-    return HttpResponse("<h1>hello</h1>")
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
