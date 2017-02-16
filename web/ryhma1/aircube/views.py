899bb1f64edebae1bf659bb349575c8877daf1d6
t5tuja02 <t5tuja02@students.oamk.fi>
2017-02-16 10:22:48 +0200

    Added basic website

--- web/ryhma1/aircube/views.py
+++ web/ryhma1/aircube/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request) :
-    return HttpResponse("<h1>hello</h1>")
+    return HttpResponse(request, 'aircube/index.html')
